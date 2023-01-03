8a9639693850e94fc1fe837b9d86aa99196446a7
mariaruth1 <113031776+mariaruth1@users.noreply.github.com>
2023-01-03 15:20:02 +0100

    initial commit

---
+++ src/PrivateMovieCollection.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author The Chosen Ones
+ */
+public class PrivateMovieCollection extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+
+    }
+
+
+    public static void main (String[] args) {
+        launch();
+    }
+}
