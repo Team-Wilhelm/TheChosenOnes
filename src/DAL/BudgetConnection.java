1216edaf71aa1dcd1c36bea9c73c68ee5aad4bc2
mariaruth1 <113031776+mariaruth1@users.noreply.github.com>
2023-01-06 11:44:30 +0100

    BudgetConnection actually for real

--- src/DAL/BudgetConnection.java
+++ src/DAL/BudgetConnection.java
@@ -1,4 +1,29 @@
 package DAL;
 
+import java.sql.Connection;
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+
 public class BudgetConnection {
+
+    /**
+     * Creates a connection to our database.
+     * @return database connection.
+     */
+    public Connection getConnection() {
+        SQLServerDataSource ds;
+        ds = new SQLServerDataSource();
+        ds.setDatabaseName("CSe22B_17_MyTunes");
+        ds.setUser("CSe22B_17");
+        ds.setPassword("CSe22B_17");
+        ds.setServerName("10.176.111.31");
+        ds.setPortNumber(1433);
+        ds.setTrustServerCertificate(true);
+        try {
+            return ds.getConnection();
+        } catch (SQLServerException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
