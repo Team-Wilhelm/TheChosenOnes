93548432944300e499015584a413a2a61347ea56
mariaruth1 <113031776+mariaruth1@users.noreply.github.com>
2023-01-06 13:03:53 +0100

    MainView is perfect just as she is

--- src/BudgetFlix.java
+++ src/BudgetFlix.java
@@ -1,5 +1,10 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 /**
  *
@@ -10,7 +15,13 @@ public class BudgetFlix extends Application {
     @Override
     public void start(Stage stage) throws Exception
     {
-
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("GUI/view/MainView.fxml")));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setTitle("BudgetFlix");
+        stage.centerOnScreen();
+        stage.show();
     }
 
 
