b0db40750392f71f4c2ec6a4ccda6b949cb9ad0a
mariaruth1 <113031776+mariaruth1@users.noreply.github.com>
2023-01-09 12:27:17 +0100

    Observable list of movies created

--- src/BLL/LogicManager.java
+++ src/BLL/LogicManager.java
@@ -1,4 +1,31 @@
 package BLL;
 
+import BE.Movie;
+import DAL.MovieDAO;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class LogicManager {
+
+    private final ObservableList<Movie> allMovies;
+    MovieDAO movieDAO = new MovieDAO();
+
+
+    public LogicManager() {
+        allMovies = FXCollections.observableArrayList();
+    }
+
+    public void getAllMovies(){
+        allMovies.clear();
+        allMovies.addAll(movieDAO.getAllMovies());
+    }
+
+    /**
+     * @return an observable list with all movies added.
+     */
+    public ObservableList<Movie> getMovieList(){
+        getAllMovies();
+        return allMovies;
+    }
+
 }
