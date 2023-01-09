5fea6bb67f42979bc1569ec536abee90f017c349
Julia Ilasova <you@example.com>
2023-01-09 14:09:20 +0100

    added stringToDoubleConverter to handle wrong rating input, finished the saveMovie method within NewMovieController

--- src/GUI/controller/NewMovieController.java
+++ src/GUI/controller/NewMovieController.java
@@ -1,9 +1,11 @@
 package GUI.controller;
 
+import BE.Movie;
 import GUI.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
@@ -11,13 +13,14 @@ import javafx.stage.Stage;
 
 import java.io.File;
 import java.nio.file.Paths;
+import java.time.LocalDate;
 
 public class NewMovieController {
     @FXML
     private TextField txtTitle, txtUserRating, txtIMDBRating, txtFilePath;
     @FXML
     private DatePicker dateLastView;
-    private Model model = null;
+    private Model model = new Model(); //TODO set to null afterwards
     private boolean isEditing = false;
 
     public void setModel(Model model){
@@ -29,8 +32,33 @@ public class NewMovieController {
         isEditing = false;
     }
 
-    public void btnSaveAction(ActionEvent actionEvent) {
+    @FXML
+    private void btnSaveAction(ActionEvent actionEvent) {
+        String title = txtTitle.getText().trim();
+        String filepath = txtFilePath.getText().trim();
+        String userRatingString = txtUserRating.getText().trim();
+        String imdbRatingString = txtIMDBRating.getText().trim();
+        LocalDate lastView = null;
 
+        if (title.isEmpty() || filepath.isEmpty()){
+            if (title.isEmpty())
+                txtTitle.setPromptText("Field must not be empty!");
+            if (filepath.isEmpty())
+                txtFilePath.setPromptText("Field must not be empty!");
+        }
+        else{
+            double userRating = stringToDoubleConverter(userRatingString);
+            double imdbRating = stringToDoubleConverter(imdbRatingString);
+            if (dateLastView.getValue() != null)
+                lastView = dateLastView.getValue();
+            if (isEditing)
+                model.editMovie(new Movie(title, filepath, lastView, imdbRating, userRating));
+            else
+                model.createMovie(new Movie(title, filepath, lastView, imdbRating, userRating));
+
+            Node node = (Node) actionEvent.getSource();
+            node.getScene().getWindow().hide();
+        }
     }
 
     @FXML
@@ -39,7 +67,8 @@ public class NewMovieController {
         node.getScene().getWindow().hide();
     }
 
-    public void btnChooseAction(ActionEvent actionEvent) {
+    @FXML
+    private void btnChooseAction(ActionEvent actionEvent) {
         Stage stage = new Stage();
         FileChooser fileChooser = new FileChooser();
         File file;
@@ -58,5 +87,33 @@ public class NewMovieController {
         if (file != null) {
             txtFilePath.setText(file.getAbsolutePath());
         }
+    }
+
+    private double stringToDoubleConverter(String rating){
+        double result = 0;
+        if (rating.isEmpty())
+            return 0;
+        else{
+            try {
+                result = Double.parseDouble(rating);
+            }
+            catch (NumberFormatException ex){
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setContentText("Invalid rating! \nPlease put in a number between 0.0 and 10.0");
+                alert.showAndWait();
+            }
+        }
+        return result;
+    }
+
+    public void setIsEditing(){
+        isEditing = true;
+        Movie movieToEdit = model.getMovieToEdit();
+        txtTitle.setText(movieToEdit.getName());
+        txtFilePath.setText(movieToEdit.getFileLink());
+        txtIMDBRating.setText(String.valueOf(movieToEdit.getImdbRating()));
+        txtUserRating.setText(String.valueOf(movieToEdit.getUserRating()));
+        dateLastView.setValue(movieToEdit.getLastView());
     }
 }
--- src/GUI/model/Model.java
+++ src/GUI/model/Model.java
@@ -1,4 +1,20 @@
 package GUI.model;
 
+import BE.Movie;
+
 public class Model {
+    Movie movieToEdit;
+    public void editMovie(Movie movie) {
+    }
+
+    public void createMovie(Movie movie) {
+    }
+
+    public void setMovieToEdit(Movie movie){
+        this.movieToEdit = movie;
+    }
+
+    public Movie getMovieToEdit() {
+        return movieToEdit;
+    }
 }
